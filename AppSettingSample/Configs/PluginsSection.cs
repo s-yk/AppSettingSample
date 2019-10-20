255cb5a2870c3a70c2e8e43a919306bfe03f5700
s-yk <sonic_cave@hotmail.co.jp>
2019-10-20 13:37:42 +0900

    Initial Commit

---
+++ AppSettingSample/Configs/PluginsSection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Configuration;
+
+namespace AppSettingSample.Configs
+{
+    public class PluginsSection : ConfigurationSection
+    {
+        public static PluginsSection GetSection()
+        {
+            return ConfigurationManager.GetSection("plugins") as PluginsSection;
+        }
+
+        [ConfigurationProperty("prePlugins")]
+        public PluginCollection PrePlugins
+        {
+            get { return base["prePlugins"] as PluginCollection; }
+        }
+
+        [ConfigurationProperty("postPlugins")]
+        public PluginCollection PostPlugins
+        {
+            get { return base["postPlugins"] as PluginCollection; }
+        }
+    }
+
+    [ConfigurationCollection(typeof(PluginElement), AddItemName = "plugin")]
+    public class PluginCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new PluginElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((PluginElement)element).AssemblyName;
+        }
+    }
+
+    public class PluginElement : ConfigurationElement
+    {
+        [ConfigurationProperty("assemblyName")]
+        public string AssemblyName {
+            get
+            {
+                return base["assemblyName"] as string;
+            }
+            set
+            {
+                this["assemblyName"] = value;
+            }
+        }
+    }
+}
---
+++ AppSettingSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using AppSettingSample.Configs;
+
+namespace AppSettingSample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var pluginsSection = PluginsSection.GetSection();
+
+            var pre = pluginsSection.PrePlugins;
+            show(pre);
+
+            var post = pluginsSection.PostPlugins;
+            show(post);
+        }
+
+        private static void show(PluginCollection cec)
+        {
+            foreach(var ce in cec)
+            {
+                var pe = ce as PluginElement;
+                Console.WriteLine(pe.AssemblyName);
+            }
+        }
+    }
+}
